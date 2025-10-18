364f4902e70042ef0116e95f4166c91168b46ec9
kamertonbuild <kamertonbuild2007@yandex.ru>
2025-10-18 04:34:15 +0300

    Настройка репликации через код

--- unit_1/src/main/java/ru/yandex/course/kafka/unit1/Replication.java
+++ unit_1/src/main/java/ru/yandex/course/kafka/unit1/Replication.java
@@ -15,11 +15,26 @@ public class Replication {
     public static void main(String[] args) {
         Properties props = new Properties();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9081");
+        /**
+         * Определяет уровень подтверждений от брокеров. Значение all требует,
+         * чтобы все реплики синхронно подтвердили получение сообщения.
+         * Только после этого сообщение считается успешно отправленным.
+         */
         props.put(ProducerConfig.ACKS_CONFIG, "all"); // Для синхронной репликации
+
+        /**
+         * Задаёт количество повторных попыток при отправке сообщений, если возникает ошибка ― например,
+         * если брокер временно недоступен.
+         */
+        props.put(ProducerConfig.RETRIES_CONFIG, 3);
+
+        /**
+         * Указывает минимальное число реплик, которые должны быть в синхронном состоянии
+         * и подтвердить получение сообщения для выполнения успешной записи.
+         */
+        props.put(TopicConfig.MIN_IN_SYNC_REPLICAS_CONFIG, "2"); // Минимум 2 реплики должны подтвердить запись
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        props.put(ProducerConfig.RETRIES_CONFIG, 3);
-        props.put(TopicConfig.MIN_IN_SYNC_REPLICAS_CONFIG, "2"); // Минимум 2 реплики должны подтвердить запись
 
         try {
             KafkaProducer<String, String> producer = new KafkaProducer<>(props);
