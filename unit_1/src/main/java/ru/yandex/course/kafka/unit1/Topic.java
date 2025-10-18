fe0c1ccbf87a22b87361e9459bef24b4c556ebee
kamertonbuild <kamertonbuild2007@yandex.ru>
2025-10-18 03:06:30 +0300

    Создание и конфигурирование топиков: успешное подключение в kafka внутри docker'а.

--- unit_1/src/main/java/ru/yandex/course/kafka/unit1/Topic.java
+++ unit_1/src/main/java/ru/yandex/course/kafka/unit1/Topic.java
@@ -8,16 +8,16 @@ import org.apache.kafka.clients.admin.NewTopic;
 /**
  * <a href="https://practicum.yandex.ru/learn/kafka/courses/83ca50bc-5dce-42cd-b629-997c971f1765/sprints/713562/topics/cfde3128-d6eb-4b4e-ba60-a0999d162a79/lessons/d41fce9c-73cd-4b74-bade-b3765cf9b984/">Создание и конфигурирование топиков</>
  */
-public class Main {
+public class Topic {
 
     public static void main(String[] args) {
-        try (Admin admin = Admin.create(Collections.singletonMap("bootstrap.servers", "localhost:9091"))) {
-            NewTopic newTopic = new NewTopic("passage-event", 1, (short) 1);
+        try (Admin admin = Admin.create(Collections.singletonMap("bootstrap.servers", "localhost:9082"))) {
+            NewTopic newTopic = new NewTopic("passage32", 1, (short) 1);
             admin.createTopics(Collections.singleton(newTopic)).all().get();
-            System.out.println("Topic 'passage-event' created successfully!");
+            System.out.println("Topic 'passage1' created successfully!");
 
         } catch (ExecutionException | InterruptedException e) {
-            e.getStackTrace();
+            System.err.println("=======> " +  e.getMessage());
         }
 
     }
--- unit_1/src/main/java/ru/yandex/course/kafka/unit1/add_topic.bat
+++ unit_1/src/main/java/ru/yandex/course/kafka/unit1/add_topic.bat
@@ -1,2 +1,2 @@
-docker exec -it broker-0 /bin/kafka-topics --create --topic organization --bootstrap-server localhost:9091 --partitions 1 --replication-factor 1
+docker exec -it broker-0 /bin/kafka-topics --create --topic organization --bootstrap-server localhost:9080 --partitions 1 --replication-factor 1
 pause
