ad792c64add014348fa5f9aa98147c86b6caa83e
kamertonbuild <kamertonbuild2007@yandex.ru>
2025-10-18 04:31:00 +0300

    Создание и конфигурирование топиков: успешное подключение в kafka внутри docker'а.

--- unit_1/src/main/java/ru/yandex/course/kafka/unit1/Topic.java
+++ unit_1/src/main/java/ru/yandex/course/kafka/unit1/Topic.java
@@ -7,6 +7,8 @@ import org.apache.kafka.clients.admin.NewTopic;
 
 /**
  * <a href="https://practicum.yandex.ru/learn/kafka/courses/83ca50bc-5dce-42cd-b629-997c971f1765/sprints/713562/topics/cfde3128-d6eb-4b4e-ba60-a0999d162a79/lessons/d41fce9c-73cd-4b74-bade-b3765cf9b984/">Создание и конфигурирование топиков</>
+ *
+ * @see <a href=https://github.com/provectus/kafka-ui/issues/4089>Not all brokers are shown in the brokers page </a>
  */
 public class Topic {
 
@@ -17,7 +19,7 @@ public class Topic {
             System.out.println("Topic 'passage1' created successfully!");
 
         } catch (ExecutionException | InterruptedException e) {
-            System.err.println("=======> " +  e.getMessage());
+            System.err.println("=======> " + e.getMessage());
         }
 
     }
